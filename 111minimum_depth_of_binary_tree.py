8b5156b2457dc9d0089c76cf7f4065cdc9516b42
Apocrypse <lucastang1994@gmail.com>
2018-02-27 12:37:01 -0500

    Initial commit

---
+++ 111minimum_depth_of_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left:
+            return 1+self.minDepth(root.right)
+        if not root.right:
+            return 1+self.minDepth(root.left)
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
