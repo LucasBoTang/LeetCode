51b49201278090d7eb0f293adf125554ac03f159
Apocrypse <lucastang1994@gmail.com>
2018-03-04 19:05:13 -0500

    Initial commit

---
+++ 257binary_tree_paths.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        result = []
+        if not root:
+            return result
+        if not root.left and not root.right:
+            result.append(str(root.val))
+            return result
+        left = self.binaryTreePaths(root.left)
+        right = self.binaryTreePaths(root.right)
+        for path in left:
+            result.append(str(root.val) + '->' + path)
+        for path in right:
+            result.append(str(root.val) + '->' + path)
+        return result
