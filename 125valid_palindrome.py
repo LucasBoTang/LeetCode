335d8b415a37e15a84870d8d4c5f2a71b79abfb9
Apocrypse <lucastang1994@gmail.com>
2018-03-05 13:41:27 -0500

    Initial commit

---
+++ 125valid_palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        slist = [word.lower() for word in s if word.isalnum()]
+        return slist == slist[::-1]
