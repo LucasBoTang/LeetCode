fbf60d02bc4188aa2884e5abd43325f96d70a51f
Apocrypse <lucastang1994@gmail.com>
2018-03-05 23:11:21 -0500

    Initial commit

---
+++ 102binary_tree_level_order_traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+        if not root:
+            return result
+        queue = [root]
+        while queue:
+            result.append([node.val for node in queue])
+            new_queue = []
+            for node in queue:
+                if node.left:
+                    new_queue.append(node.left)
+                if node.right:
+                    new_queue.append(node.right)
+            queue = new_queue
+        return result
---
+++ 261graph_valid_tree.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def validTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+        if len(edges) != n - 1:
+            return False
+
+        graph = {i: set() for i in range(n)}
+        for j, k in edges:
+            graph[j].add(k)
+            graph[k].add(j)
+        print(graph)
+
+        queue = [0]
+        hash = {0}
+
+        while queue:
+            new_queue = []
+            for node in queue:
+                for neighbor in graph[node]:
+                    if neighbor in hash:
+                        continue
+                    hash.add(neighbor)
+                    new_queue.append(neighbor)
+            queue = new_queue
+
+        return len(hash) == n
