62ca556176be93f677828addc7e5e6fb8e205bbe
Apocrypse <lucastang1994@gmail.com>
2018-03-07 20:42:40 -0500

    Initial commit

---
+++ 025reverse_nodes_in_k_group.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        dummy, dummy.next = ListNode(0), head
+        prev = dummy
+        while prev is not None:
+            prev = self.reverseNextKNodes(prev, k)
+        return dummy.next
+
+    def reverseNextKNodes(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        # find kth node
+        cur, first_node = head, head.next
+        for _ in range(k):
+            cur = cur.next
+            while cur is None:
+                return None
+        kth_node = cur
+        kth_next = cur.next
+        # reverse
+        prev, cur = head, head.next
+        while cur != kth_next:
+            temp, cur.next = cur.next, prev
+            prev, cur = cur, temp
+        head.next, first_node.next = kth_node, kth_next
+        return first_node
---
+++ 206reverse_linked_list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev, cur = None, head
+        while cur:
+            temp, cur.next = cur.next, prev
+            prev, cur = cur, temp
+        return prev
