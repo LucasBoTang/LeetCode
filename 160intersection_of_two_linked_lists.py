03e79c66d3f64e345b98d9a39c7bfe5b0bcd48ce
Apocrypse <lucastang1994@gmail.com>
2018-03-10 11:58:44 -0500

    Initial commit

---
+++ 160intersection_of_two_linked_lists.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        while not headA or not headB:
+            return None
+        lenA = self.getNodelen(headA)
+        lenB = self.getNodelen(headB)
+        dif = abs(lenA - lenB)
+        if lenA > lenB:
+            for _ in range(dif):
+                headA = headA.next
+        else:
+            for _ in range(dif):
+                headB = headB.next
+        while headA and headB:
+            if headA == headB:
+                return headA
+            headA = headA.next
+            headB = headB.next
+        return None
+
+    def getNodelen(self, head):
+        """
+        :type head: ListNode
+        :rtype: int
+        """
+        len = 0
+        while head:
+            len += 1
+            head = head.next
+        return len
