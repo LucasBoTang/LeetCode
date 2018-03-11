7877417dace252268735e6cec763ad915ad80a49
Apocrypse <lucastang1994@gmail.com>
2018-03-10 19:36:17 -0500

    Initial commit

---
+++ 120triangle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        while not triangle:
+            return 0
+        n = len(triangle)
+        for i in range(n+1):
+            for j in range(n-i-1):
+                triangle[n-i-2][j] =  triangle[n-i-2][j] + min(triangle[n-i-1][j], triangle[n-i-1][j+1])
+        return triangle[0][0]
