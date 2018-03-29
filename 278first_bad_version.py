c8a5be06ec550f66374500944a94bfd179bab725
Apocrypse <lucastang1994@gmail.com>
2018-03-29 13:06:45 -0400

    Initial commit

---
+++ 278first_bad_version.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start, end = 1, n
+        while start < end:
+            mid = start + (end - start) // 2
+            if isBadVersion(mid):
+                end = mid
+            else:
+                start = mid + 1
+        return start
