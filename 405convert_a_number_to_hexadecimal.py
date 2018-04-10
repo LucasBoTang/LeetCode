cb85b8415666d7ed8a503957157ef693f652fe7c
Apocrypse <lucastang1994@gmail.com>
2018-04-10 16:51:47 -0400

    Initial commit

--- 405convert_a_number_to_hexadecimal.py
+++ 405convert_a_number_to_hexadecimal.py
@@ -7,7 +7,7 @@ class Solution:
         if not num:
             return '0'
         result = ''
-        hexmap = '0 1 2 3 4 5 6 7 8 9 a b c d e f'.split()
+        hexmap = '0123456789abcdef'
         while num < 0:
             num += 16 ** 8
         while num:
