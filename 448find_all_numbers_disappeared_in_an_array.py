18afd6d603b35f693ec6f87766409fdc1919eacc
Apocrypse <lucastang1994@gmail.com>
2018-04-21 11:07:39 -0400

    Initial commit

---
+++ 448find_all_numbers_disappeared_in_an_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = [0] + nums
+        for i in range(len(nums)):
+            index = abs(nums[i])
+            nums[index] = -abs(nums[index])
+        return [i for i in range(len(nums)) if nums[i] > 0]
