3a0b2a191e79f1a8cc06749a8c67a37f739cf578
Apocrypse <lucastang1994@gmail.com>
2018-04-23 13:21:52 -0400

    Initial commit

---
+++ 455assign_cookies.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        result = 0
+        g.sort()
+        s.sort()
+        i, j = 0, 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                result += 1
+                i += 1
+            j += 1
+        return result
