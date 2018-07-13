743b14a31502d8544c2b657e70edfff954901b9c
Apocrypse <lucastang1994@gmail.com>
2018-07-13 00:09:59 -0400

    Initial commit

---
+++ 559maximum_depth_of_n-ary_tree.py
@@ -0,0 +1,17 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return 1 + max([self.maxDepth(child) for child in root.children] + [0])
