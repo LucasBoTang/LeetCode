13b9b0311acf5b14f6184da108dd0d1e0277f823
Apocrypse <lucastang1994@gmail.com>
2018-07-23 10:27:41 -0400

    Initial commit

---
+++ 589n-ary_tree_preorder_traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        result = [root.val]
+        for child in root.children:
+            result += self.preorder(child)
+        return result
