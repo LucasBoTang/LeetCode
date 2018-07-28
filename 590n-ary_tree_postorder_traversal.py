18b867334595935c7e1cc612986d4730600da5f7
Apocrypse <lucastang1994@gmail.com>
2018-07-28 08:06:08 -0400

    Initial commit

---
+++ 590n-ary_tree_postorder_traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+
+        result = []
+
+        if not root:
+            return result
+
+        for child in root.children:
+            result += self.postorder(child)
+        result.append(root.val)
+
+        return result
