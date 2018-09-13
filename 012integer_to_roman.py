cba068c8db0560bb1ae090f43d4a4198c8d4975e
Apocrypse <lucastang1994@gmail.com>
2018-09-13 15:29:37 -0400

    Initial commit

---
+++ 012integer_to_roman.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        result = ''
+
+        for i in range(13):
+            if num // values[i]:
+                result += num // values[i] * symbols[i]
+                num %= values[i]
+
+        return result
