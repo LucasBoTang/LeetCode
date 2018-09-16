e1458cee34962752b5947cc9c41c11e214c58182
Apocrypse <lucastang1994@gmail.com>
2018-09-15 21:35:53 -0400

    Initial commit

--- 017letter_combinations_of_a_phone_number.py
+++ 017letter_combinations_of_a_phone_number.py
@@ -5,11 +5,12 @@ class Solution:
         :rtype: List[str]
         """
 
+        result = []
         if not digits:
-            return []
+            return result
 
         maps = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-        result = self.dfs(digits, '', maps, [])
+        result = self.dfs(digits, '', maps, result)
         return result
 
 
