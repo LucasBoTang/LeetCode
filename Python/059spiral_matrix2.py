f7783e2eefbd45ca554e31e01b704a2d41fa5ef1
Apocrypse <lucastang1994@gmail.com>
2019-01-28 21:13:25 +0100

    Add 059

---
+++ Python/059spiral_matrix2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+
+        layers = (n + 1) // 2
+        result = [[0 for _ in range(n)] for _ in range(n)]
+
+        num = 1
+        for l in range(layers):
+            print(l)
+            # top
+            for i in range(l, n-l-1):
+                result[l][i] = num
+                num += 1
+            # right
+            for i in range(l, n-l-1):
+                result[i][n-l-1] = num
+                num += 1
+            # bottom
+            for i in range(n-l-1, l, -1):
+                result[n-l-1][i] = num
+                num += 1
+            # left
+            for i in range(n-l-1, l, -1):
+                result[i][l] = num
+                num += 1
+
+        if n % 2 == 1:
+            result[n//2][n//2] = num
+
+        return result
