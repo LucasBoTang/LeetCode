a91726b4ab678650b55aff64d1a7f92026be69d9
Apocrypse <lucastang1994@gmail.com>
2019-02-12 20:05:49 +0100

    Add 071

---
+++ Python/071simplify_path.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+
+        result = []
+        path_list = path.split('/')
+
+        for s in path_list:
+            if not s or s == '.':
+                continue
+            elif s == '..':
+                if result:
+                    result.pop()
+            else:
+                result.append(s)
+
+        return '/' + '/'.join(result)
