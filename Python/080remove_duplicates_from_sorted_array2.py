1da50701be7d2d998e79e6e4d95915b9a3ff5d1b
Apocrypse <lucastang1994@gmail.com>
2019-02-19 21:11:26 +0100

    Add 080

---
+++ Python/080remove_duplicates_from_sorted_array2.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        length = 1
+        cnt = 1
+        prev = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == prev:
+                cnt += 1
+            else:
+                prev = nums[i]
+                cnt = 1
+            if cnt <= 2:
+                nums[length] = nums[i]
+                length += 1
+
+        return length
