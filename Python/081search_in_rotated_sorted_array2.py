c62bde7020d692f8b3dc7e2332b07d5547a68af9
Apocrypse <lucastang1994@gmail.com>
2019-02-20 23:22:07 +0100

    Add 081

---
+++ Python/081search_in_rotated_sorted_array2.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+
+        start, end = 0, len(nums) - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+
+            if nums[mid] == target:
+                return True
+
+            while start < mid and nums[start] == nums[mid]:
+                start += 1
+
+            if nums[start] <= nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+        return False
