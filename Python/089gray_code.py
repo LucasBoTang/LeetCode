c0732d54a889ce6f77ce640bfc9c2c6be9950412
Apocrypse <lucastang1994@gmail.com>
2019-02-24 11:19:50 +0100

    Add 089

---
+++ Python/089gray_code.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = [0]
+
+        if n == 0:
+            return result
+
+        for i in range(1, n + 1):
+            new = [code + 2 ** (i - 1) for code in result][::-1]
+            result += new
+
+        return result
