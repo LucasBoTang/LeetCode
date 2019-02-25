699b9e6aab93b76d94d01e55b9cf3896ac3c432c
Apocrypse <lucastang1994@gmail.com>
2019-02-25 21:28:29 +0100

    Add 090

---
+++ Python/090subsets2.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]
+        nums.sort()
+        self.dfs([], nums, result)
+
+        return result
+
+
+    def dfs(self, cur, nums, result):
+        """
+        :type cur: List[int]
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            result
+
+        for i in range(len(nums)):
+            new = cur + [nums[i]]
+
+            if new not in result:
+                result.append(new)
+                result = self.dfs(new, nums[i+1:], result)
+
+        return result
