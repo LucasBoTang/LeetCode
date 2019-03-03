0f62f390816e1b1afce3c1ea19e7428a6b6da430
Apocrypse <lucastang1994@gmail.com>
2019-03-03 13:42:06 +0100

    Add 092

---
+++ Python/092reverse_linked_list2.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        prev, cur = None, head
+        for i in range(1, m):
+            prev, cur = cur, cur.next
+
+        left, right = prev, cur
+
+        prev = None
+        for i in range(n - m + 1):
+            temp, cur.next = cur.next, prev
+            prev, cur = cur, temp
+
+        if left:
+            left.next = prev
+        else:
+            head = prev
+        right.next = cur
+
+        return head
