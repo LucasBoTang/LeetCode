d49ba2ea99a6acbe06ca8ee29aa25b5539662042
Apocrypse <lucastang1994@gmail.com>
2019-03-05 22:13:30 +0100

    Add 94

---
+++ Python/094binary_tree_inorder_traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+
+        if not root:
+            return result
+
+        result += self.inorderTraversal(root.left)
+        result.append(root.val)
+        result += self.inorderTraversal(root.right)
+
+        return result
