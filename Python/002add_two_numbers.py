9d9373229e15372db99878e70420f3454b271d4f
Bo Tang <lucastang1994@gmail.com>
2023-10-24 19:13:35 -0400

    Update 002add_two_numbers.py

--- Python/002add_two_numbers.py
+++ Python/002add_two_numbers.py
@@ -1,33 +1,30 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        dummy = ListNode(None)
-        cur = dummy
-
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # init dummy listnode for the sum
+        cur = dummy = ListNode(None)
+        # init carry
         carry = 0
         while l1 or l2:
-            digit = carry
+            # sum together
+            sum_val = carry
             if l1:
-                digit += l1.val
+                sum_val += l1.val
                 l1 = l1.next
             if l2:
-                digit += l2.val
+                sum_val += l2.val
                 l2 = l2.next
-            cur.next = ListNode(digit % 10)
-            carry = digit // 10
+            # carry           
+            carry, sum_val = divmod(sum_val, 10)
+            # append linked list
+            cur.next = ListNode(val=sum_val)
             cur = cur.next
-
+        # carry for the last digit
         if carry:
-            cur.next = ListNode(1)
-
+            cur.next = ListNode(val=carry)
         return dummy.next
+            
