bdc2eecbcf4ea053a6c1a6a42a9dc49c694d2c29
Bo Tang <lucastang1994@gmail.com>
2023-10-24 00:19:20 -0400

    Update 001two_sum.py

--- Python/001two_sum.py
+++ Python/001two_sum.py
@@ -1,13 +1,7 @@
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_dict = {}
-        for index, num in enumerate(nums):
+        for ind, num in enumerate(nums):
             if target - num in nums_dict:
-                return [nums_dict[target - num], index]
-            nums_dict[num] = index
-        return
+                return nums_dict[target - num], ind
+            nums_dict[num] = ind
