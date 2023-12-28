214b754d1e50103592470e3356a4bb0163bec6f2
Bo Tang <lucastang1994@gmail.com>
2023-12-28 16:58:04 -0500

    Update 209minimum_size_subarray_sum.py

--- Python/209minimum_size_subarray_sum.py
+++ Python/209minimum_size_subarray_sum.py
@@ -1,3 +1,4 @@
+# Binary Search O(nlogn)
 class Solution(object):
     def minSubArrayLen(self, target, nums):
         """
@@ -25,4 +26,31 @@ class Solution(object):
                 size = min(size, start - i + 1)
             elif psums[end + 1]  - psums[i] >= target:
                 size = min(size, end - i + 1)
+        return size if size != float("inf") else 0
+
+# Two Pointers O(n)
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        # init subarray sum
+        sub_sum = 0
+        # init min size
+        size = float("inf")
+        # init end pointer
+        end = 0
+        # move start pointer
+        for start in range(len(nums)):
+            while end < len(nums) and sub_sum < target:
+                # move end pointer
+                sub_sum += nums[end]
+                end += 1
+            # sum greater than target
+            if sub_sum >= target:
+                size = min(size, end - start)
+            # start with next number
+            sub_sum -= nums[start]
         return size if size != float("inf") else 0
