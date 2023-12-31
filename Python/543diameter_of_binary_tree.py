cbfe065c73df6bac8d5dd1d9b5efa08eb052b465
Bo Tang <lucastang1994@gmail.com>
2023-12-31 14:44:35 -0500

    Update 543diameter_of_binary_tree.py

--- Python/543diameter_of_binary_tree.py
+++ Python/543diameter_of_binary_tree.py
@@ -1,27 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
     def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if not root:
-            return 0
-        return max(1+self.findLongestPath(root.left)+1+self.findLongestPath(root.right), \
-                   self.diameterOfBinaryTree(root.left), \
-                   self.diameterOfBinaryTree(root.right))
+        max_diam, max_len = self.dfs(root)
+        return max_diam
 
-    def findLongestPath(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+    # dfs
+    def dfs(self, root):
+        # base case
         if not root:
-            return -1
-        return 1 + max(self.findLongestPath(root.left), self.findLongestPath(root.right))
+            return 0, 0
+        # recursion
+        max_diam_left, max_len_left = self.dfs(root.left)
+        max_diam_right, max_len_right = self.dfs(root.right)
+        max_diam = max(max_len_left + max_len_right, 
+                       max_diam_left,
+                       max_diam_right)
+        max_len = 1 + max(max_len_left, max_len_right)
+        return max_diam, max_len
