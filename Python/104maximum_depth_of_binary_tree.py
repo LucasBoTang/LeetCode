cb051c69ee061ecb500bed48637f0a4bedfd3dac
Bo Tang <lucastang1994@gmail.com>
2023-12-31 14:27:13 -0500

    Update 104maximum_depth_of_binary_tree.py

--- Python/104maximum_depth_of_binary_tree.py
+++ Python/104maximum_depth_of_binary_tree.py
@@ -5,12 +5,24 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
     def maxDepth(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if root:
-            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        return 0
+        # base case
+        if root is None:
+            return 0
+        # recursion case
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return 1 + max(left_depth, right_depth)
+        
