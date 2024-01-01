f0e72de0cbe099ae75c82072829e596f9f84c645
Bo Tang <lucastang1994@gmail.com>
2024-01-01 12:16:36 -0500

    Update 049group_anagrams.py

--- Python/049group_anagrams.py
+++ Python/049group_anagrams.py
@@ -1,11 +1,11 @@
-class Solution:
+class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        result = collections.defaultdict(list)
+        anag_dict = collections.defaultdict(list)
         for s in strs:
-            key =  "".join(sorted(s))
-            result[key].append(s)
-        return result.values()
+            key = "".join(sorted(s))
+            anag_dict[key].append(s)
+        return anag_dict.values()
