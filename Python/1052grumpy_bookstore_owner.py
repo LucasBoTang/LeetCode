a66cb172f8e337bd2d8c00bdb753caefa578a77d
Bo Tang <lucastang1994@gmail.com>
2024-01-04 15:27:38 -0500

    Update 1052grumpy_bookstore_owner.py

--- Python/1052grumpy_bookstore_owner.py
+++ Python/1052grumpy_bookstore_owner.py
@@ -6,13 +6,18 @@ class Solution(object):
         :type minutes: int
         :rtype: int
         """
-
-        # satisfication wihout technique
-        original_satisfied = sum([(1 - g) * c for c, g in zip(customers, grumpy)])
         # addtional customers that can be satisfied
         additional_satisfied = [g * c for c, g in zip(customers, grumpy)]
         # init slide window
-        cur_satisfied = original_satisfied + sum(additional_satisfied[:minutes])
+        cur_satisfied = 0
+        for i in range(len(customers)):
+            # in the window
+            if i < minutes:
+                cur_satisfied += customers[i]
+            # not grumpy
+            elif not grumpy[i]:
+                cur_satisfied += customers[i]
+        # init max
         max_satisfied = cur_satisfied
         # slide window
         for i in range(minutes, len(customers)):
