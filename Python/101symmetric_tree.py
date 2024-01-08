4f7adba55ffbd94d266ca85f490cf0ed8b34afd7
Bo Tang <lucastang1994@gmail.com>
2024-01-07 20:41:10 -0500

    Update 101symmetric_tree.py

--- Python/101symmetric_tree.py
+++ Python/101symmetric_tree.py
@@ -1,28 +1,30 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        if root:
-            return self.isSym(root.left, root.right)
-        return True
+        # empty tree
+        if not root:
+            return True
+        # dfs
+        return self.isSym(root.left, root.right)
 
     def isSym(self, left, right):
-        """
-        :type left: TreeNode
-        :type right: TreeNode
-        :rtype: bool
-        """
-        if right == None and left == None:
+        # base case
+        if not left and not right:
+            # leaf node
             return True
-        if right and left and left.val == right.val:
-            return self.isSym(left.left, right.right) and self.isSym(left.right, right.left)
-        return False
+        if not left or not right or left.val != right.val:
+            # asymetric children
+            return False
+        # recursion case
+        outer = self.isSym(left.left, right.right)
+        inner = self.isSym(left.right, right.left)
+        return outer and inner
