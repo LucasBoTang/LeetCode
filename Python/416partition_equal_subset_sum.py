8d5750ed0721ab05f056489fab5e25a266483d8c
Bo Tang <lucastang1994@gmail.com>
2024-01-10 03:29:19 -0500

    Update 416partition_equal_subset_sum.py

--- Python/416partition_equal_subset_sum.py
+++ Python/416partition_equal_subset_sum.py
@@ -6,23 +6,18 @@ class Solution(object):
         """
         nums_sum = sum(nums)
         # if the sum is odd, always false
-        if nums_sum % 2 :
+        if nums_sum % 2:
             return False
         # use dp to find nums_sum // 2
         target = nums_sum //  2
         # init dp table
-        dp_table = [[False] * (target + 1) for _ in range(len(nums))]
-        dp_table[0][0] = True
-        if nums[0] <= target:
-            dp_table[0][nums[0]] = True
+        dp_table = [True] + [False] * target
         # add num by iteration
-        for i in range(1, len(nums)):
-            n = nums[i]
+        for n in nums:
             # get sum of partition
-            for j in range(target+1):
-                # update dp table
-                dp_table[i][j] =  dp_table[i-1][j]
+            for t in range(target, -1, -1): # reversed order to avoid duplication
                 # sum less than target
-                if j - n >= 0:
-                    dp_table[i][j] |= dp_table[i-1][j-n]
-        return dp_table[-1][-1]
+                if t - n >= 0:
+                    # update dp table
+                    dp_table[t] |= dp_table[t-n]
+        return dp_table[-1]
