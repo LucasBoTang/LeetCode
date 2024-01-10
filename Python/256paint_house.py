f65c88cedbacdbcd1597d977f36df76778938c5c
Bo Tang <lucastang1994@gmail.com>
2024-01-10 02:15:37 -0500

    Update 256paint_house.py

--- Python/256paint_house.py
+++ Python/256paint_house.py
@@ -1,10 +1,20 @@
-class Solution:
+class Solution(object):
     def minCost(self, costs):
         """
         :type costs: List[List[int]]
         :rtype: int
         """
-        cur = [0] * 3
-        for next in costs:
-            cur = [next[i] + min(cur[:i] + cur[i+1:]) for i in range(3)]
-        return min(cur)
+        # init dp memo for the first house
+        r, g, b = costs[0]
+        # iterate by painting the next house
+        for cost_r, cost_g, cost_b in costs[1:]:
+            # update previous costs
+            prev_r, prev_g, prev_b = r, g, b
+            # paint red
+            r = cost_r + min(prev_g, prev_b)
+            # paint green
+            g = cost_g + min(prev_r, prev_b)
+            # paint blue
+            b = cost_b + min(prev_r, prev_g)
+        # get the minimum cost
+        return min(r, g, b)
