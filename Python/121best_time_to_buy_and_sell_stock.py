6978002fcfaf6f1a44ea6aacdfa9615b1524845e
Bo Tang <lucastang1994@gmail.com>
2024-01-15 03:47:40 -0500

    Update 121best_time_to_buy_and_sell_stock.py

--- Python/121best_time_to_buy_and_sell_stock.py
+++ Python/121best_time_to_buy_and_sell_stock.py
@@ -1,16 +1,16 @@
- class Solution(object):
+class Solution(object):
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        # init min price
-        min_price = prices[0]
         # init max profit
         max_profit = 0
-        # selling
-        for p in prices:
+        # init min price
+        min_price = prices[0]
+        for p in prices[1:]:
+            # get min prices to buy a stock
             min_price = min(min_price, p)
-            profit = p - min_price
-            max_profit = max(max_profit, profit)
+           # sell stock with lowest buying price
+            max_profit = max(max_profit, p - min_price)
         return max_profit
