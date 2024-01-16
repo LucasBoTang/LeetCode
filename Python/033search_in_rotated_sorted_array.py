3062e60fa7137c5a265ec3252c1a14e9e66135b6
Bo Tang <lucastang1994@gmail.com>
2024-01-16 02:58:48 -0500

    Update 033search_in_rotated_sorted_array.py

--- Python/033search_in_rotated_sorted_array.py
+++ Python/033search_in_rotated_sorted_array.py
@@ -1,29 +1,34 @@
-class Solution:
+class Solution(object):
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-
+        # init pointers
         start, end = 0, len(nums) - 1
-
+        # binary search
         while start <= end:
+            # mid pointer
             mid = start + (end - start) // 2
-
+            # find target
             if nums[mid] == target:
                 return mid
-
-            if nums[mid] < nums[start]:
-                if target < nums[mid] or target >= nums[start]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-
-            else:
+            # left is sorted
+            if nums[mid] >= nums[start]:
+                # keep left
                 if target >= nums[start] and target < nums[mid]:
                     end = mid - 1
+                # keep right
                 else:
                     start = mid + 1
-
+            # right is sorted
+            else:
+                # keep right
+                if target > nums[mid] and target <= nums[end]:
+                    start = mid + 1
+                # keep left
+                else:
+                    end = mid - 1
+        # cannot find target
         return -1
