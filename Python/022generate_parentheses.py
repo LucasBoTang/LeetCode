57e83d4d893cbde1d752e6fa4faeb9688be90344
Bo Tang <lucastang1994@gmail.com>
2024-01-18 09:09:05 -0500

    Update 022generate_parentheses.py

--- Python/022generate_parentheses.py
+++ Python/022generate_parentheses.py
@@ -1,28 +1,24 @@
-class Solution:
+class Solution(object):
     def generateParenthesis(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
-        result = []
-        self.addParenthesis('', 0, 0, n, result)
-        return result
+        # init result
+        res = []
+        # dfs
+        self.dfs("", 0, 0, n, res)
+        return res
 
-
-    def addParenthesis(self, solution, left, right, n, result):
-        """
-        :type solution: str
-        :type n: int
-        :type left: int
-        :type right: int
-        :rtype: List[str]
-        """
-
-        if right + left == 2 * n:
-            result.append(solution)
+    def dfs(self, solution, num_left, num_right, n, res):
+        # base case: find a combination
+        if num_left == n and num_right == n:
+            res.append(solution)
             return
-
-        if left < n:
-            self.addParenthesis(solution+'(', left+1, right, n, result)
-        if right < left:
-            self.addParenthesis(solution+')', left, right+1, n, result)
+        # recursion case:
+        if num_left < n:
+            # add left parenthesis
+            self.dfs(solution+"(", num_left+1, num_right, n, res)
+        if num_left > num_right:
+            # add righht parenthesis
+            self.dfs(solution+")", num_left, num_right+1, n, res)
